2ca1e73ed77fb94da9f825cf82894d909d34597f
Kun <kun.situ@hotmail.com>
2018-09-29 19:42:20 +0100

    Improve the get_all_name functionality

--- gsheet/gsheet.py
+++ gsheet/gsheet.py
@@ -3,7 +3,7 @@ import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 import pygsheets
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # change this client_secret to your client secret(Keep it safe), which can be obtained google API wesite.
 # And there are a lot of tutorial for this part.
@@ -14,17 +14,23 @@ class google_df(object):
         gc = pygsheets.authorize(outh_file=client_secret)
         print('Pygsheet Autorisation Done')
         self.ps = gc
-
-    def get_all_name(self, verbose = 'name', show = 10):
-        # get all the spreadsheet name and ID
         info = self.ps.list_ssheets()
+        self.sheetlist = list(map(lambda x: x['name'], info))
+        self.sheetlist_id = list(map(lambda x: (x['name'], x['id']), info))
+        
+    def get_all_name(self, verbose = 'name', show = 10):
+        separator = '-'*20
+        # get all the spreadsheet name and ID
+        print(f'Number of spreadsheet in the drive: {len(self.sheetlist)}')
+        print(f'The first {show} are \n {separator}')
         if verbose == 'name':
-            for a, b in enumerate(list(map(lambda x: x['name'], info))[:show]):
-                print('{} Name: {}'.format(a+1, b))
+            for a, b in enumerate(self.sheetlist[:show]):
+                print(f'{a+1} Name: {b}')
         elif verbose == 'both':
-            for a, b in enumerate(list(map(lambda x: (x['name'], x['id']), info))[:show]):
-                print('{} Name: {}\nID:{}\n{}'.format(a+1, b[0], b[1], '-'*20))
-        else: raise Exception('verbose error')
+            for a, b in enumerate(self.sheetlist_id[:show]):
+                print(f'{a+1} Name: {b[0]} \n ID: {b[1]} \n {separator}')
+        else: 
+            raise Exception('verbose error')
 
     def get_all_sheetname(self, filename):
         file1 = self.ps.open(filename).worksheets()
@@ -43,13 +49,12 @@ class google_df(object):
         # Open workbook
         file1 = self.ps.open(filename)
         if show == True:
-            print('All the sheets in the workbook: {}'\
-            .format(file1.worksheets()))
+            print(f'All the sheets in the workbook: {file1.worksheets()}')
         else: pass
 
         def add_work_sheet(sheetname, row = '200', col='200'):
             file1.add_worksheet(sheetname,rows=row,cols=col)
-            return print('sheet: {} is added/n rows = {}/n col = {}'.format(sheetname, row, col))
+            return print(f'sheet: {sheetname} is added/n rows = {row}/n col = {col}')
         return file1
     
     def sheet2df(self, filename, sheetname):
